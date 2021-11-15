ea788e860cc19907117e32358757257aaed5ac3c
Vikram Kalta <vikram.kalta@e9.co.uk>
2021-11-15 22:45:25 +0530

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+// markup
+const AboutPage = () => {
+  return (
+    <div>
+      hello world (about)
+      <Link to="/">Go home</Link>.
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/new.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+// markup
+const NewPage = () => {
+  return (
+    <div>
+      hello world (new)
+      <Link to="/">Go home</Link>.
+    </div>
+  )
+}
+
+export default NewPage
