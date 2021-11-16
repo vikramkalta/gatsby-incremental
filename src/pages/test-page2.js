839142efc38c06abbfc68d21d16e7dc1ee6d47da
Vikram Kalta <vikram.kalta@e9.co.uk>
2021-11-16 11:11:18 +0530

    test: added 3 new page

--- src/pages/test-page2.js
+++ src/pages/test-page2.js
@@ -2,17 +2,15 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 // markup
-const IndexPage = () => {
+const TestPage2 = () => {
   return (
     <div style={{display:'flex',flexDirection:'column'}}>
-      hello world
+      Test page 2
       <Link to="/about">Go about</Link>.
       <Link to="/new">Go new</Link>.
-      <Link to="/test-page1">Go test-page1</Link>.
-      <Link to="/test-page2">Go test-page2</Link>.
-      <Link to="/test-page3">Go test-page3</Link>.
+      <Link to="/home">Go home</Link>.
     </div>
   )
 }
 
-export default IndexPage
+export default TestPage2
