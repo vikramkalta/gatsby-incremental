b9d21036529cf137e0f870b2e4d2999fcd6b934a
Vikram Kalta <vikram.kalta@e9.co.uk>
2021-11-16 20:55:05 +0530

    test: minor change

--- src/pages/test-page1.js
+++ src/pages/test-page1.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const TestPage1 = () => {
   return (
     <div style={{display:'flex',flexDirection:'column'}}>
-      Test page 1
+      Test page 1.
       <Link to="/about">Go about</Link>.
       <Link to="/new">Go new</Link>.
       <Link to="/home">Go home</Link>.
